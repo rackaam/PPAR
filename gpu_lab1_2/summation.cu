c93946edbeb1fb98c7cd75331b64c449d93f3d6b
rakam <matthias@rakam.eu>
2015-03-25 11:55:57 +0100

    return one value per block

--- gpu_lab1_2/summation.cu
+++ gpu_lab1_2/summation.cu
@@ -35,25 +35,9 @@ float reverse_log2_series(int n)
 	return sum;
 }
 
-int main(int argc, char ** argv)
+void basic_gpu_summation(int data_size)
 {
-    int data_size = 1024 * 1024 * 128, i;
-
-    // Run CPU version
-    double start_time = getclock();
-    float log2 = log2_series(data_size);
-    double end_time = getclock();
-
-    double r_start_time = getclock();
-    float r_log2 = reverse_log2_series(data_size);
-    double r_end_time = getclock();
-    
-    printf(" log(2)=\n%.15f\n", log(2.0));
-    printf("CPU result:\n%.15f\n", log2);
-    printf("CPU result (reverse):\n%.15f\n", r_log2);
-    printf(" time=%fs\n", end_time - start_time);
-    printf(" time=%fs (reverse)\n", r_end_time - r_start_time);
-    
+	int i;
     // Parameter definition
     int threads_per_block = 16 * 32;
     int blocks_in_grid = 8;
@@ -110,6 +94,91 @@ int main(int argc, char ** argv)
   
     CUDA_SAFE_CALL(cudaEventDestroy(start));
     CUDA_SAFE_CALL(cudaEventDestroy(stop));
+}
+
+void reduced_gpu_summation(int data_size)
+{
+	int i;
+    // Parameter definition
+    int threads_per_block = 16 * 32;
+    int blocks_in_grid = 8;
+
+    // Timer initialization
+    cudaEvent_t start, stop;
+    CUDA_SAFE_CALL(cudaEventCreate(&start));
+    CUDA_SAFE_CALL(cudaEventCreate(&stop));
+
+	// Allocating output data on GPU
+    float *d_C;
+	cudaMalloc((void **)&d_C, blocks_in_grid * sizeof(float));
+
+	// Shared memory size
+	int smemSize = threads_per_block * sizeof(float);
+
+    // Start timer
+    CUDA_SAFE_CALL(cudaEventRecord(start, 0));
+
+    // Execute kernel
+    reduced_summation_kernel<<<blocks_in_grid, threads_per_block, smemSize>>>(data_size, d_C);
+
+    // Stop timer
+    CUDA_SAFE_CALL(cudaEventRecord(stop, 0));
+    CUDA_SAFE_CALL(cudaEventSynchronize(stop));
+
+    // Get results back
+    float* res = (float*)malloc(blocks_in_grid * sizeof(float));
+	cudaMemcpy(res, d_C, blocks_in_grid * sizeof(float), cudaMemcpyDeviceToHost);
+	
+    // Finish reduction
+	float sum = 0.;
+	for(i = 0; i < blocks_in_grid; i ++) {
+		sum += res[i];
+	}
+    
+    // Cleanup
+	cudaFree(d_C);
+	free(res);
+    
+    printf("Reduced GPU results:\n");
+    printf(" Sum: %.15f\n", sum);
+    
+    float elapsedTime;
+    CUDA_SAFE_CALL(cudaEventElapsedTime(&elapsedTime, start, stop));	// In ms
+
+    double total_time = elapsedTime / 1000.;	// s
+    double time_per_iter = total_time / (double)data_size;
+    double bandwidth = sizeof(float) / time_per_iter; // B/s
+    
+    printf(" Total time: %g s,\n Per iteration: %g ns\n Throughput: %g GB/s\n",
+    	total_time,
+    	time_per_iter * 1.e9,
+    	bandwidth / 1.e9);
+  
+    CUDA_SAFE_CALL(cudaEventDestroy(start));
+    CUDA_SAFE_CALL(cudaEventDestroy(stop));
+}
+
+int main(int argc, char ** argv)
+{
+    int data_size = 1024 * 1024 * 128;
+
+    // Run CPU version
+    double start_time = getclock();
+    float log2 = log2_series(data_size);
+    double end_time = getclock();
+
+    double r_start_time = getclock();
+    float r_log2 = reverse_log2_series(data_size);
+    double r_end_time = getclock();
+    
+    printf(" log(2)=\n%.15f\n", log(2.0));
+    printf("CPU result:\n%.15f\n", log2);
+    printf("CPU result (reverse):\n%.15f\n", r_log2);
+    printf(" time=%fs\n", end_time - start_time);
+    printf(" time=%fs (reverse)\n", r_end_time - r_start_time);
+    
+	reduced_gpu_summation(data_size);
+
     return 0;
 }
 
--- gpu_lab1_2/summation_kernel.cu
+++ gpu_lab1_2/summation_kernel.cu
@@ -1,5 +1,5 @@
 
-// GPU kernel
+//data_out : tableau avec une case pour le résultat de chacun des threads
 __global__ void summation_kernel(int data_size, float * data_out)
 {
 	int nbThread = gridDim.x * blockDim.x;
@@ -15,7 +15,7 @@ __global__ void summation_kernel(int data_size, float * data_out)
 	data_out[idx] = sum;
 }
 
-// GPU kernel
+//data_out : tableau avec une case pour le résultat de chacun des threads
 __global__ void summation_kernel2(int data_size, float * data_out)
 {
 	int nbThread = gridDim.x * blockDim.x;
@@ -34,4 +34,44 @@ __global__ void summation_kernel2(int data_size, float * data_out)
 	data_out[idx] = sum;
 }
 
+//data_out : tableau avec une case pour le résultat de chacun des blocks
+__global__ void reduced_summation_kernel(int data_size, float * data_out)
+{
+	extern __shared__ float sdata[];
 
+	unsigned int tid = threadIdx.x;
+	unsigned int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+	// Summation
+	int nbThread = gridDim.x * blockDim.x;
+	int nbElts = data_size / nbThread;
+	int firstElt = id * nbElts;
+	int lastElt = (id + 1) * nbElts;
+	int i;
+	float sum = 0;
+	for(i = firstElt; i < lastElt; i++) {
+		if (i % 2 == 0)
+			sum += 1.0 / (i + 1);
+		else
+			sum -= 1.0 / (i + 1);
+	}
+	sdata[tid] = sum;
+
+	// Attente des résulatts de chaque thread dans sdata
+	__syncthreads();
+
+	// Reduction
+	for (unsigned int s = 1; s < blockDim.x; s *= 2) {
+		int index = 2 * s * tid;
+		if(index < blockDim.x) {
+			sdata[index] += sdata[index + s];
+		}
+		// Attente de la fin des calculs avant de passer au niveau de réduction suivant
+		__syncthreads();
+	}
+
+	// Quand la réduction est terminé, le résultat se trouve dans la première case de la mémoire partagée
+	// Un thread par block écrit le résultat
+	if (tid == 0) 
+		data_out[blockIdx.x] = sdata[0];
+}
